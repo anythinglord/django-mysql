07c0d98d7cd5ce315395ee072ea0f39aac6821db
anythinglord <sjsalcedoa@unal.edu.co>
2023-10-11 17:33:45 -0500

    create signup and home views, add the feature to create users

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return render(request, 'signup.html',{
-        'form': UserCreationForm()
-    })
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+            'form': UserCreationForm()
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'],
+                    password=request.POST['password1']
+                )
+                user.save()
+                return HttpResponse('User created')
+            except:
+                return HttpResponse('User already exists')
+
+        return HttpResponse('Passwords don`t match')
+    
