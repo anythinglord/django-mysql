d2fc38eab92ded73fe315d149cebe5a7d6995b1e
anythinglord <sjsalcedoa@unal.edu.co>
2023-10-11 17:52:47 -0500

    create new tasks view, use render to show an specific error

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Django CRUD & Mysql
+
+hands on the REST API 
+
+* create a python virtual env 
+python -m venv MYENV
+
+* activate the current venv
+source ./path/the/venv
+
+* run the app
+python manage.py runserver
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
 def signup(request):
     if request.method == 'GET':
-        return render(request, 'signup.html',{
-            'form': UserCreationForm()
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
         })
     else:
         if request.POST['password1'] == request.POST['password2']:
@@ -20,9 +24,18 @@ def signup(request):
                     password=request.POST['password1']
                 )
                 user.save()
-                return HttpResponse('User created')
-            except:
-                return HttpResponse('User already exists')
+                login(request, user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'User already exists'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Passwords don`t match'
+        })
 
-        return HttpResponse('Passwords don`t match')
-    
+
+def tasks(request):
+    return render(request, 'tasks.html')
