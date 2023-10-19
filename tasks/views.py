b314eecbd1e16522c625ee03424521188649f85a
anythinglord <sjsalcedoa@unal.edu.co>
2023-10-19 16:28:37 -0500

    create view & form for the new taks

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
-
+from .forms import TaskForm
 # Create your views here.
 
 
@@ -40,6 +40,15 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
+    else:
+        return render(request, 'create_task.html', {
+            'form': TaskForm
+        })
 
 def signout(request):
     logout(request)
