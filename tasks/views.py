64e4c48146cdd1f05eeb0482f1fb65007e6cbb9b
anythinglord <sjsalcedoa@unal.edu.co>
2023-11-02 15:41:39 -0500

    add delete and complete button in the detail page

--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
 # Create your views here.
 
 
@@ -39,7 +40,7 @@ def signup(request):
 
 
 def tasks(request):
-    tasks = Task.objects.filter(user=request.user)
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'tasks.html', {
         'tasks': tasks
     })
@@ -61,15 +62,21 @@ def create_task(request):
                 'form': TaskForm,
                 'error': 'There is a invalida value'
             })
-        
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
+
 def task_detail(request, task_id):
     if request.method == 'GET':
-        task = get_object_or_404(Task, pk=task_id)
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
         form = TaskForm(instance=task)
         return render(request, 'task_detail.html', { 'task': task, 'form': form })
     else:
         try:
-            task = get_object_or_404(Task, pk=task_id)
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
             form = TaskForm(request.POST, instance=task)
             form.save()
             return redirect('tasks')
@@ -78,6 +85,13 @@ def task_detail(request, task_id):
                 'task': task, 'form': form,
                 'error': 'Error updating task'
             })
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted =  timezone.now()
+        task.save()
+        return redirect('tasks')
 
 def signout(request):
     logout(request)
