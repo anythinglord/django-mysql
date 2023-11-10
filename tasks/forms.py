24e5e3a16bf6a9d502856fc1d326bed1ceda647c
anythinglord <sjsalcedoa@unal.edu.co>
2023-11-09 21:10:50 -0500

    add styles using bootstrap5

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Task
 
-class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Write your username'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(attrs={'class': 'form-check-input m-auto'})
+        }
